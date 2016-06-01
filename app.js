252c0bfee058e27124dc3f1a6482bf54888f18f7
Josh Kuhar <jdkuhar@gmail.com>
2016-06-01 17:08:13 -0400

    commit message

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 $(".add-item-button").click(function() {
-	var item = $(".add-item-input").val();
-	$(".food-list").append('<li>' +item+ '</li>'); 
+	$(".food-list").append('<li>' +$(".add-item-input").val()+ '</li>'); 
+	$(".add-item-input").empty();
 	//console.log("check");
 	});
 
-/*$(".add-item-button").click(function() {
-	$(".add-item-input").val();
-	$(".food-list").append(); 
-	//console.log("check");
-	});*/
+/*$(".remove-item-btn").click(function() {
+	$("li").empty().parent();
+	console.log("check");
+});*/
 
+$(".remove-item-btn").click(function() {
+	$("li").remove()/*.parent(".remove-item-btn")*/;
+	console.log("check");
+});
+
+/*$('food-list').on('click', 'li', function(event){
+	$("li").val().remove();
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+});*/
