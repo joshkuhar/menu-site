27725911672f8e4a7cc8af67365d39ab627686c8
Josh Kuhar <jdkuhar@gmail.com>
2016-06-03 15:17:34 -0400

    commit message

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ $(".add-item-button").click(function() {
 	console.log("check");
 	});
 
+// use enter to add list items
+$('.add-item-input').keyup(function(event){
+	if(event.keyCode == 13) {
+		//event.preventDefault();
+		$('.add-item-button').click();
+	};
+});	
+
 $('ul').on('click', '.remove-item-btn', function(event){
   $(this).parent().remove();
   console.log("remove clicked");
@@ -23,4 +31,4 @@ $('ul').on('click', '.remove-item-btn', function(event){
 	//$("li").val().remove();
   // fires when any LIs are clicked on
   // including LIs that aren't on the page when it is initially loaded
-});*/
+});*/	
