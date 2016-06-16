dad0322e4dd4d42097c1368a0430c04f05e4090e
Josh Kuhar <jdkuhar@gmail.com>
2016-06-16 13:06:29 -0400

    update

--- app.js
+++ app.js
@@ -16,19 +16,3 @@ $('ul').on('click', '.remove-item-btn', function(event){
   $(this).parent().remove();
   console.log("remove clicked");
 });
-
-/*$('ul').on('click', '.trash', function(event){
-	console.log('trash clicked');
-	$(this).closest('li').remove();
-}); */
-
-/*$(".remove-item-btn").click(function() {
-	$(this).parent().remove();
-	console.log("check");
-});*/
-
-/*$('food-list').on('click', 'li', function(event){
-	//$("li").val().remove();
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
-});*/	
